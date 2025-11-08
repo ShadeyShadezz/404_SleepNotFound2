fea6832855d82ac65c35fb6002cecd7111ac7a12
Yara Kemeh <yarascarlet45@gmail.com>
2025-11-07 19:12:04 -0500

    remove old folder

--- my-app/src/components/LoginCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function LoginCard({ onLogin }) {
-  return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Welcome to StudyBuddy!</h1>
-      <p>Your personal study companion.</p>
-      <button onClick={onLogin}>Log In</button>
-    </div>
-  );
-}
